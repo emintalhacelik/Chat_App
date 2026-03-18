85cf72e27d5afcd2635b5c9797b386e423f7ae8e
emint <emint@MSI>
2026-03-18 18:09:02 +0300

    Proje dosyası ekle.

---
+++ Chat_App/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Chat_App
+{
+    public partial class Form1 : Form
+    {
+        TcpClient istemci;
+        ListBox lstChat;
+        TextBox txtMesaj;
+        Button btnBaglan, btnGonder;
+
+        public Form1()
+        {
+            this.Text = "Chat_App";
+            this.Size = new Size(400, 500);
+            this.BackColor = Color.WhiteSmoke;
+
+            // Kodla Arayüz Elemanları
+            btnBaglan = new Button() { Text = "BAĞLAN", Location = new Point(12, 12), Size = new Size(360, 35), BackColor = Color.Teal, ForeColor = Color.White };
+            btnBaglan.Click += (s, e) => {
+                try
+                {
+                    istemci = new TcpClient("127.0.0.1", 9595);
+                    MessageBox.Show("Chat_App'a Giriş Yapıldı!");
+                }
+                catch { MessageBox.Show("Sunucu kapalı!"); }
+            };
+
+            lstChat = new ListBox() { Location = new Point(12, 60), Size = new Size(360, 300) };
+            txtMesaj = new TextBox() { Location = new Point(12, 380), Size = new Size(260, 30) };
+            btnGonder = new Button() { Text = "GÖNDER", Location = new Point(280, 378), Size = new Size(90, 25), BackColor = Color.SeaGreen, ForeColor = Color.White };
+            btnGonder.Click += BtnGonder_Click;
+
+            this.Controls.Add(btnBaglan);
+            this.Controls.Add(lstChat);
+            this.Controls.Add(txtMesaj);
+            this.Controls.Add(btnGonder);
+        }
+
+        private void BtnGonder_Click(object sender, EventArgs e)
+        {
+            if (istemci != null && istemci.Connected && !string.IsNullOrEmpty(txtMesaj.Text))
+            {
+                NetworkStream stream = istemci.GetStream();
+                byte[] veri = Encoding.UTF8.GetBytes(txtMesaj.Text);
+                stream.Write(veri, 0, veri.Length);
+                lstChat.Items.Add("Siz: " + txtMesaj.Text);
+                txtMesaj.Clear();
+            }
+        }
+    }
+}
---
+++ Sunucu/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Sunucu
+{
+    public partial class Form1 : Form
+    {
+        // Nesneleri kodla tanımlıyoruz
+        TcpListener server;
+        TcpClient client;
+        ListBox lstMesajlar;
+        Button btnBaslat;
+
+        public Form1()
+        {
+            this.Text = "Chat_App Sunucu";
+            this.Size = new Size(400, 450);
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            // Buton
+            btnBaslat = new Button();
+            btnBaslat.Text = "SUNUCUYU BAŞLAT";
+            btnBaslat.Size = new Size(360, 40);
+            btnBaslat.Location = new Point(12, 12);
+            btnBaslat.BackColor = Color.LightGreen;
+            btnBaslat.Click += BtnBaslat_Click;
+            this.Controls.Add(btnBaslat);
+
+            // Listeler
+            lstMesajlar = new ListBox();
+            lstMesajlar.Size = new Size(360, 320);
+            lstMesajlar.Location = new Point(12, 60);
+            this.Controls.Add(lstMesajlar);
+        }
+
+        private void BtnBaslat_Click(object sender, EventArgs e)
+        {
+            server = new TcpListener(IPAddress.Any, 9595);
+            server.Start();
+            lstMesajlar.Items.Add(">> Sunucu dinleniyor... (Port: 9595)");
+
+            Thread dinle = new Thread(MesajBekle);
+            dinle.Start();
+        }
+
+        void MesajBekle()
+        {
+            client = server.AcceptTcpClient();
+            lstMesajlar.Items.Add(">> Bir kullanıcı bağlandı!");
+
+            while (client.Connected)
+            {
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int okunan = stream.Read(buffer, 0, buffer.Length);
+                    if (okunan > 0)
+                    {
+                        string mesaj = Encoding.UTF8.GetString(buffer, 0, okunan);
+                        lstMesajlar.Items.Add("Gelen: " + mesaj);
+                    }
+                }
+                catch { break; }
+            }
+        }
+    }
+}
